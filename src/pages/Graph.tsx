496d8f66b0ae53c1be8ab9b4e8dbb91ea10222a1
Yoon JongBeom <home99032@naver.com>
2024-06-01 14:04:36 +0900

    fix: api 수정

--- src/pages/Graph.tsx
+++ src/pages/Graph.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import Header from "../components/Header";
-import Filter from "../components/Filter";
-import GraphView from "../components/GraphView";
-import Simulation from "../components/Simulation";
-import SimulationItem from "../components/SimulationItem";
-import { getData } from "../api/getData";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import Filter from '../components/Filter';
+import GraphView from '../components/GraphView';
+import Simulation from '../components/Simulation';
+import SimulationItem from '../components/SimulationItem';
+import { getData } from '../api/getData';
+import { useLocation } from 'react-router-dom';
 interface TemperatureData {
 	year: number;
 	average?: number;
@@ -23,7 +23,7 @@ export default function Graph() {
 	const unit: number[] = [1, 5, 10, 20];
 	const [selectedStartYear, setSelectedStartYear] = useState<number>(1900);
 	const [selectedPeriod, setSelectedPeriod] = useState<number>(5);
-	const [selectedView, setSelectedView] = useState<string>("전체 보기");
+	const [selectedView, setSelectedView] = useState<string>('전체 보기');
 
 	const handleStartYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		setSelectedStartYear(Number(e.target.value));
@@ -56,7 +56,7 @@ export default function Graph() {
 		const initData = async (data: any, newName: string) => {
 			let name = data.location || `${data.latitude}°N ${data.longitude}°E`;
 			if (newName) name = newName;
-			const response = await getData("temperature", data);
+			const response = await getData('temperature', data);
 			console.log(response);
 			const filterDataByYear = (
 				data: TemperatureData[],
@@ -68,16 +68,16 @@ export default function Graph() {
 			};
 
 			const high: any[] = [
-				...filterDataByYear(response.observeds, "average"),
-				...filterDataByYear(response.predicteds, "highest"),
+				...filterDataByYear(response.observeds, 'average'),
+				...filterDataByYear(response.predicteds, 'highest'),
 			];
 			const aver: any[] = [
-				...filterDataByYear(response.observeds, "average"),
-				...filterDataByYear(response.predicteds, "average"),
+				...filterDataByYear(response.observeds, 'average'),
+				...filterDataByYear(response.predicteds, 'average'),
 			];
 			const low: any[] = [
-				...filterDataByYear(response.observeds, "average"),
-				...filterDataByYear(response.predicteds, "lowest"),
+				...filterDataByYear(response.observeds, 'average'),
+				...filterDataByYear(response.predicteds, 'lowest'),
 			];
 			const color = getRandomColor();
 			setData((prevData: any) => [
@@ -87,49 +87,38 @@ export default function Graph() {
 		};
 		const getTwoData = async () => {
 			const queryParams = new URLSearchParams(location.search);
-			const locations = queryParams.get("name");
-			const latitude = queryParams.get("latitude");
-			const longitude = queryParams.get("longitude");
-			const qssp = queryParams.get("ssp");
-			const qseason = queryParams.get("season");
-			const ssp = [
-				"SSP1-1.9",
-				"SSP1-2.6",
-				"SSP2-4.5",
-				"SSP3-7.0",
-				"SSP4-3.4",
-				"SSP4-6.0",
-				"SSP5-3.4",
-				"SSP5-8.5",
-			];
-			const season = ["연평균", "봄", "여름", "가을", "겨울"];
-			const selectedSsp = ssp.indexOf(qssp!);
-			const selectedSeason = season.indexOf(qseason!);
+			const locations = queryParams.get('name');
+			const latitude = queryParams.get('latitude');
+			const longitude = queryParams.get('longitude');
+			const qssp = queryParams.get('ssp');
+			const qseason = queryParams.get('season');
+			const selectedSsp = qssp;
+			const selectedSeason = qseason;
 
 			const data1: any = {};
 			const area = [
-				"전 세계",
-				"아시아",
-				"유럽",
-				"아프리카",
-				"북아메리카",
-				"남아메리카",
-				"오세아니아",
-				"남극",
+				'전 세계',
+				'아시아',
+				'유럽',
+				'아프리카',
+				'북아메리카',
+				'남아메리카',
+				'오세아니아',
+				'남극',
 			];
 			if (locations && area.includes(locations)) data1.location = locations;
 			if (latitude) data1.latitude = latitude;
 			if (longitude) data1.longitude = longitude;
-			if (ssp) data1.ssp = selectedSsp;
-			if (season) data1.season = selectedSeason;
+			if (selectedSsp) data1.ssp = selectedSsp;
+			if (selectedSeason) data1.season = selectedSeason;
 
 			await initData(
 				{
-					location: "전 세계(기준)",
-					ssp: 0,
-					season: 0,
+					location: '전 세계(기준)',
+					ssp: 'SSP1-1.9',
+					season: '연평균',
 				},
-				""
+				''
 			);
 			await initData(data1, locations!);
 		};
@@ -157,7 +146,7 @@ export default function Graph() {
 			const filterLow = set.low
 				.filter((item: any) => yearList.includes(item.year))
 				.map((item: any) => item.value);
-			if (selectedView === "전체 보기") {
+			if (selectedView === '전체 보기') {
 				dataset.push({
 					label: `${set.name} 최고`,
 					data: filterHigh,
@@ -167,7 +156,7 @@ export default function Graph() {
 					borderWidth: 0.5,
 					pointRadius: 1.5,
 					pointHitRadius: 3,
-					spanGaps : 10,
+					spanGaps: 10,
 				});
 				dataset.push({
 					label: `${set.name}`,
@@ -178,7 +167,7 @@ export default function Graph() {
 					borderWidth: 2,
 					pointRadius: 1.5,
 					pointHitRadius: 3,
-					spanGaps : 10,
+					spanGaps: 10,
 				});
 				dataset.push({
 					label: `${set.name} 최저`,
@@ -189,9 +178,9 @@ export default function Graph() {
 					borderWidth: 0.5,
 					pointRadius: 1.5,
 					pointHitRadius: 3,
-					spanGaps : 10,
+					spanGaps: 10,
 				});
-			} else if (selectedView === "평균 보기") {
+			} else if (selectedView === '평균 보기') {
 				dataset.push({
 					label: set.name,
 					data: filterAver,
@@ -201,7 +190,7 @@ export default function Graph() {
 					borderWidth: 2,
 					pointRadius: 1.5,
 					pointHitRadius: 3,
-					spanGaps : 10,
+					spanGaps: 10,
 				});
 			}
 		});
@@ -222,35 +211,23 @@ export default function Graph() {
 
 	const handleSimulationSubmit = async (newData: any) => {
 		let response;
-		console.log(newData);
-		const ssp = [
-			"SSP1-1.9",
-			"SSP1-2.6",
-			"SSP2-4.5",
-			"SSP3-7.0",
-			"SSP4-3.4",
-			"SSP4-6.0",
-			"SSP5-3.4",
-			"SSP5-8.5",
-		];
-		const season = ["연평균", "봄", "여름", "가을", "겨울"];
-		const selectedSsp = ssp.indexOf(newData.selectedSsp);
-		const selectedSeason = season.indexOf(newData.selectedSeason);
-		if (newData.selectedArea === "") {
+		const selectedSsp = newData.selectedSsp;
+		const selectedSeason = newData.selectedSeason;
+		if (newData.selectedArea === '') {
 			const requestData = {
 				latitude: newData.latitude,
 				longitude: newData.longitude,
 				ssp: selectedSsp,
 				season: selectedSeason,
 			};
-			response = await getData("temperature", requestData);
+			response = await getData('temperature', requestData);
 		} else {
 			const requestData = {
 				location: newData.selectedArea,
 				ssp: selectedSsp,
 				season: selectedSeason,
 			};
-			response = await getData("temperature", requestData);
+			response = await getData('temperature', requestData);
 		}
 
 		const filterDataByYear = (
@@ -264,19 +241,19 @@ export default function Graph() {
 		};
 
 		const high: any[] = [
-			...filterDataByYear(response.observeds, "average"),
-			...filterDataByYear(response.predicteds, "highest"),
+			...filterDataByYear(response.observeds, 'average'),
+			...filterDataByYear(response.predicteds, 'highest'),
 		];
 		const aver: any[] = [
-			...filterDataByYear(response.observeds, "average"),
-			...filterDataByYear(response.predicteds, "average"),
+			...filterDataByYear(response.observeds, 'average'),
+			...filterDataByYear(response.predicteds, 'average'),
 		];
 		const low: any[] = [
-			...filterDataByYear(response.observeds, "average"),
-			...filterDataByYear(response.predicteds, "lowest"),
+			...filterDataByYear(response.observeds, 'average'),
+			...filterDataByYear(response.predicteds, 'lowest'),
 		];
 		let name: any;
-		if (newData.selectedArea === "") {
+		if (newData.selectedArea === '') {
 			name = `${newData.latitude}°N ${newData.longitude}°E`;
 		} else {
 			name = newData.selectedArea;
@@ -297,7 +274,7 @@ export default function Graph() {
 		);
 	};
 
-	const [isVisibleWorld, setIsVisibleWorld] = useState("표시");
+	const [isVisibleWorld, setIsVisibleWorld] = useState('표시');
 	const handleIsVisibleWorld = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		setIsVisibleWorld(e.target.value);
 	};
@@ -308,13 +285,13 @@ export default function Graph() {
 				<div className="flex flex-col items-center w-full gap-3 mt-10">
 					<div className="w-full web:w-3/4 px-20pxr web:px-0">
 						<p className="flex flex-col items-start text-3xl font-semibold text-left web:items-center web:gap-10pxr web:flex-row">
-							{queryParams.get("name")
-								? queryParams.get("name")
-								: `${queryParams.get("latitude")}°N ${queryParams.get(
-										"longitude"
-								  )}°E`}{" "}
+							{queryParams.get('name')
+								? queryParams.get('name')
+								: `${queryParams.get('latitude')}°N ${queryParams.get(
+										'longitude'
+								  )}°E`}{' '}
 							<span className="text-2xl text-gray-500">
-								{queryParams.get("ssp")}, {queryParams.get("season")}
+								{queryParams.get('ssp')}, {queryParams.get('season')}
 							</span>
 						</p>
 					</div>
@@ -331,7 +308,7 @@ export default function Graph() {
 						handleIsVisibleWorld={handleIsVisibleWorld}
 					/>
 					{showData && (
-						<GraphView data={graphData} isVisible={isVisibleWorld === "표시"} />
+						<GraphView data={graphData} isVisible={isVisibleWorld === '표시'} />
 					)}
 					<Simulation onSubmit={handleSimulationSubmit} />
 					<div className="flex flex-col items-center flex-grow w-full gap-3">
